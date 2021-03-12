ea75b30406e3148e1033a39cc2f7930fd4bbc82a
jordannachris <jordannachris@gmail.com>
2021-03-11 22:01:45 -0300

    terceiro commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<v-app>
+  <AppTopBar/>
+    <v-main> 
+        <router-view/>
+    </v-main>
+</v-app>
+
+</template>
+
+<script>
+import AppTopBar from './components/shared/AppTopBar'
+
+export default {
+  name: 'App',
+
+  components: {
+    AppTopBar
+  }
+};
+</script>
